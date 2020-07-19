a470a56f2220b8048e2e36f0993c2e00fdeed65d
Robert_Wlodarczyk <robert.michal.wlodarczyk@gmail.com>
2020-07-19 11:35:06 +0200

    Zakończone zad 12

--- src/Zad12.java
+++ src/Zad12.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Zad12 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        char znakSpacji = ' ';
+        int sumSpacji = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == znakSpacji) {
+                sumSpacji++;
+            }
+
+
+        }
+
+        int procent = (int) ((double) sumSpacji / text.length() * 100);
+
+
+        System.out.println(sumSpacji + " Procent: " + procent);
+    }
 }
